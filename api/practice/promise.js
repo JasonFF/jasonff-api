39473279b267b8349700fda7c23c41b736f1d44b
JasonFF <925784658@qq.com>
2017-03-12 10:10:39 +0800

    add a article

---
+++ api/practice/promise.js
@@ -0,0 +1,95 @@
+var oneOneSecondLater = function (callback) {
+    setTimeout(function () {
+        callback(1);
+    }, 1000);
+};
+
+var maybeOneOneSecondLater = function (callback, errback) {
+    setTimeout(function () {
+        if (Math.random() < .5) {
+            callback(1);
+        } else {
+            errback(new Error("Can't provide one."));
+        }
+    }, 1000);
+};
+
+
+// ------- //
+
+var maybeOneOneSecondLater = function () {
+    var callback;
+    setTimeout(function () {
+        callback(1);
+    }, 1000);
+    return {
+        then: function (_callback) {
+            callback = _callback;
+        }
+    };
+};
+
+maybeOneOneSecondLater().then(callback);
+
+/*
+2 weaknesses
+
+- The last caller of the then method determines the callback that is used.
+- It would be more useful if every registered callback were notified of the resolution.
+
+> notify 通知
+*/
+
+
+var maybeOneOneSecondLater = function () {
+    var pending = [], value;
+    setTimeout(function () {
+        value = 1;
+        for (var i = 0, ii = pending.length; i < ii; i++) {
+            var callback = pending[i];
+            callback(value);
+        }
+        pending = undefined;
+    }, 1000);
+    return {
+        then: function (callback) {
+            if (pending) {
+                pending.push(callback);
+            } else {
+                callback(value);
+            }
+        }
+    };
+};
+
+
+var defer = function () {
+    var pending = [], value;
+    return {
+        resolve: function (_value) {
+            value = _value;
+            for (var i = 0, ii = pending.length; i < ii; i++) {
+                var callback = pending[i];
+                callback(value);
+            }
+            pending = undefined;
+        },
+        then: function (callback) {
+            if (pending) {
+                pending.push(callback);
+            } else {
+                callback(value);
+            }
+        }
+    }
+};
+
+var oneOneSecondLater = function () {
+    var result = defer();
+    setTimeout(function () {
+        result.resolve(1);
+    }, 1000);
+    return result;
+};
+
+oneOneSecondLater().then(callback);
